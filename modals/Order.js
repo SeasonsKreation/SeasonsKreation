1a97dc394baf58d1c57c1cda11dccd8ecf56c355
SeasonsKreation <168418457+SeasonsKreation@users.noreply.github.com>
2024-05-05 23:30:41 +0530

    Update Order.js

--- modals/Order.js
+++ modals/Order.js
@@ -4,6 +4,14 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema(
 	{
 		orderId: { type: String },
+		productName: { type: String,default: "Gold 18k beauty", },
+		customerName: { type: String,default: "Mustkeem", },
+		phone: { type: String,default: "67865897", },
+		email: { type: String,default: "Mustkeem", },
+		address: { type: String,default: "qwerty", },
+		pincode: { type: String,default: "302016", },
+		city: { type: String,default: "jaipur", },
+		state: { type: String,default: "Rajsthan", },
 		purchaseDate: { type: Date },
 		amount: { type: Number },
 		paymentStatus: {
