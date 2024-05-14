52da515a451ff7356cc8366e926e8745c46bc2fd
SeasonsKreation <168418457+SeasonsKreation@users.noreply.github.com>
2024-05-14 21:54:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express=require('express')
 const app=express()
+const bodyParser = require("body-parser");
 require('dotenv').config()
 
 
 
 const Adminrouter=require('./router/routerapi')
 const mongoose=require('mongoose')
-// mongoose.connect(`${process.env.DB_URl}/${process.env.DB_NAME}`)
+//mongoose.connect(`${process.env.DB_URl}/${process.env.DB_NAME}`)
 
-// const uri = "mongodb+srv://sharewithcoder:jiPbDhTcdTkjeOyi@skjewel.7eyxgqj.mongodb.net/?retryWrites=true&w=majority&appName=SKjewel";
-const uri = "mongodb+srv://seasonskreation:o5AMOlAhSyKo6WR7@cluster0.31xa1xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri)
+ const uri = "mongodb+srv://seasons:SeasonK6979@cluster0.31xa1xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+ mongoose.connect(uri)
 
 
 
@@ -18,21 +18,17 @@ mongoose.connect(uri)
 const path=require('path')
 app.use(express.json())
 var cors=require("cors")
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
+
 app.use(express.static('public'))
 app.use(cors())
 app.use('/skapi',Adminrouter)
 
 const  _dirname= path.dirname("")
 const buildpath=path.join(__dirname,"../front/build")
-
-
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-
 app.use(express.static('buildpath'))
 
 app.listen(process.env.PORT,()=>{console.log(`Yeah! Great Micky Your Server is Runing on ${process.env.PORT}`)})
